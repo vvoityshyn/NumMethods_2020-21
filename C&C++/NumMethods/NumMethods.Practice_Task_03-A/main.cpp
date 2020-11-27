a33fe592e1640f57553cad1c46eb9c24ad1bf8e4
Volodymyr Voityshyn <voytyshyn@gmail.com>
2020-11-27 13:31:32 +0200

    Practice task #3 - Trapezoid Rule

---
+++ C&C++/NumMethods/NumMethods.Practice_Task_03-A/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+double TrapezoidSquare(double x1, double x2, double f1, double f2);
+double F(double x);
+
+int main()
+{
+	cout << "Numerical Methods: Practice Task #03 / Example A" << endl;
+
+	double a = 0;
+	double b = 1;
+	int n = 100;
+	double h = (b - a) / n;
+
+	double I = 0;
+	for (int i = 0; i < n; i++)
+	{
+		double x1 = i * h;
+		double x2 = i * h + h;		
+		double s = TrapezoidSquare(x1, x2, F(x1), F(x2));
+
+		printf("x1 = %lf; x2 = %lf\n", x1, x2);
+
+		I += s;
+	}
+
+	printf("Result: %lf\n", I);
+
+	system("pause");
+	return 0;
+}
+
+double TrapezoidSquare(double x1, double x2, double f1, double f2)
+{
+	return 0.5 * (x2 - x1) * (f1 + f2);
+}
+
+double F(double x)
+{
+	return x * (1 - x * x);
+}
