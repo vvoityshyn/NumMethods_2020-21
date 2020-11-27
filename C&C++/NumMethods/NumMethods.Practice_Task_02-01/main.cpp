7f29ff1ff411815d3d5a3fe8c89004aeb7b90ed2
Volodymyr Voityshyn <voytyshyn@gmail.com>
2020-11-27 12:50:21 +0200

    Fixed Guaussian elimination procedure

--- C&C++/NumMethods/NumMethods.Practice_Task_02-01/main.cpp
+++ C&C++/NumMethods/NumMethods.Practice_Task_02-01/main.cpp
@@ -19,7 +19,12 @@ int main()
 	const int rows = 4;
 	const int cols = 5;
 	// Augmented matrix of the linear system
-	double C[rows][cols] = { {2, 1, -1, 1, 1}, {2, -1, 0, -3, 2}, {5, 1, -1, 2, -1}, {2, -1, 1, -3, 4} };	
+	double C[rows][cols] = {
+			{2, -1, 1, -1, 1},
+			{2, -1, 0, -3, 2},
+			{3, 0, -1, 1, -3},
+			{2, 2, -2, 5, -6}
+		};	
 
 	printf("Augmented matrix of a system of linear algebraic equations:\n");
 	PrintMatrix(C[0], 4, 5);
@@ -28,6 +33,27 @@ int main()
 	for (int c = 0; c < cols - 1; c++)
 	{
 		double p = C[c][c];
+		if (abs(p) < 1e-6)
+		{
+			int q = c + 1;
+			for (; q < rows; q++)
+			{
+				if (abs(C[q][c]) > 1e-6)
+				{
+					break;
+				}
+			}
+			// replace row c with row q
+			double temp;
+			for (int j = 0; j < cols; j++)
+			{
+				temp = C[c][j];
+				C[c][j] = C[q][j];
+				C[q][j] = temp;
+			}
+			PrintMatrix(C[0], rows, cols);
+		}
+		p = C[c][c];
 		for (int k = c; k < cols; k++)
 		{
 			C[c][k] = C[c][k] / p;
@@ -78,14 +104,15 @@ void PrintMatrix(double* m, int rows, int cols)
 	{
 		for (int c = 0; c < cols; c++)
 		{
-			if (abs(*m) > 1e-6)
+			printf("%lf\t", *m);
+			/*if (abs(*m) > 1e-6)
 			{ 
 				printf("%lf\t", *m);
 			}
 			else
 			{
 				printf("        \t");
-			}
+			}*/
 			
 			m++;
 		}
